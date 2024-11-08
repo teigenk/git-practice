9220b438fc24eb98b3111f6e58e1facbbf62da2d
teigenk <katharine.teigen@gmail.com>
2024-11-08 11:23:13 -0800

    changed to read_csv

--- sciadv_plots_ray2024.R
+++ sciadv_plots_ray2024.R
@@ -13,10 +13,10 @@ pacman::p_load(
   extrafont)
 
 # read file
-read.csv(dataframe)
+read_csv(dataframe)
 
 # read another one
-read.csv(dataframe2)
+read_csv(dataframe2)
 
 #getting needed font
 font_import()
